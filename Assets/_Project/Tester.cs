d7aa0f3d642cb1fa7bc310c1b15bc0ac736eccb6
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2025-01-28 16:37:28 -0700

    more tests on tester

--- Assets/_Project/Tester.cs
+++ Assets/_Project/Tester.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -6,20 +7,48 @@ public class Tester : MonoBehaviour
     private void RunTests()
     {
         TestColorToRGBCode();
+        TestPlayerCount();
+        TestMainMenuSelection();
+	}
+
+	private void TestColorToRGBCode()
+	{
+		// Arrange
+		Color expected = Color.green;
+
+		// Act
+		Color actual = Utils.PlayerColorToRGB(PlayerColors.Green);
+
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	private void TestPlayerCount()
+    {
+        // Create new board
+        BoardCreator board = new BoardCreator();
+
+        // Choose random range from 2-4 players for game
+        board.PlayerCount = Random.Range(2, 5);
+
+        // Create the board after PlayerCount is chosen
+        board.Build();
     }
 
-    private void TestColorToRGBCode()
+    private void TestMainMenuSelection()
     {
-        // Arrange
-        Color expected = Color.green;
-        
-        // Act
-        Color actual = Utils.PlayerColorToRGB(PlayerColors.Green);
-        
-        // Assert
-        Assert.AreEqual(expected, actual);
-    }
-    
-    
-    
+        // Create new menu manager
+        MainMenuManager menuManager = new MainMenuManager();
+
+        // Simulate button presses: start, back to main menu, start again
+        menuManager.StartGameButton();
+		menuManager.MainMenuBackBtn();
+		menuManager.StartGameButton();
+
+		// Choose random range from 2-4 players for game
+		menuManager.PlayerCountSelection(Random.Range(2, 5));
+
+        // Test quit button
+        menuManager.QuitGameButton();
+	}
 }
